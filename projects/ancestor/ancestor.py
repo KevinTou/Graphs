2a9787059b3f027206a324813dfc747574d36930
Kevin Tou <toukevin@gmail.com>
2020-01-22 14:56:03 -0800

    Added class solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,47 +2,47 @@ from graph import Graph
 from util import Stack, Queue
 
 
-def earliest_ancestor(ancestors, starting_node):
-    graph = Graph()
-    stack = Stack()
+# def earliest_ancestor(ancestors, starting_node):
+#     graph = Graph()
+#     stack = Stack()
 
-    visited = set()
+#     visited = set()
 
-    # Add parent and child pairs to graph as vertices
-    for parent, child in ancestors:
-        graph.add_vertex(parent)
-        graph.add_vertex(child)
+#     # Add parent and child pairs to graph as vertices
+#     for parent, child in ancestors:
+#         graph.add_vertex(parent)
+#         graph.add_vertex(child)
 
-    # Add the connections between parent and child as vertices
-    for parent, child in ancestors:
-        graph.add_edge(parent, child)
+#     # Add the connections between parent and child as vertices
+#     for parent, child in ancestors:
+#         graph.add_edge(parent, child)
 
-    # Add starting node to stack
-    stack.push(starting_node)
+#     # Add starting node to stack
+#     stack.push(starting_node)
 
-    # Initialize ancestor value with -1 (or false)
-    ancestor = -1
+#     # Initialize ancestor value with -1 (or false)
+#     ancestor = -1
 
-    # Go through the stack
-    while stack.size() > 0:
-        current = stack.pop()
-        # Go through the list of vertices and add all the parents that the current is a child of
-        for vertex in graph.vertices:
-            if current in graph.get_neighbors(vertex) and current not in visited:
-                visited.add(current)
-                stack.push(vertex)
+#     # Go through the stack
+#     while stack.size() > 0:
+#         current = stack.pop()
+#         # Go through the list of vertices and add all the parents that the current is a child of
+#         for vertex in graph.vertices:
+#             if current in graph.get_neighbors(vertex) and current not in visited:
+#                 visited.add(current)
+#                 stack.push(vertex)
 
-        # Update ancestor based on whether or not current changed (has a parent)
-        if current == starting_node:
-            ancestor = -1
-        else:
-            ancestor = current
+#         # Update ancestor based on whether or not current changed (has a parent)
+#         if current == starting_node:
+#             ancestor = -1
+#         else:
+#             ancestor = current
 
-    # Return the earliest ancestor (if there was one)
-    return ancestor
+#     # Return the earliest ancestor (if there was one)
+#     return ancestor
 
 # start: 1
-# ancester: 10
+# ancestor: 10
 
 
 '''
@@ -82,3 +82,33 @@ you run through the list without adding to the stack or queue
 # print(earliest_ancestor(test_ancestors, 9))  # 4
 # print(earliest_ancestor(test_ancestors, 10))  # -1
 # print(earliest_ancestor(test_ancestors, 11))  # -1
+
+def earliest_ancestor(ancestors, starting_node):
+    # Build the graph
+    graph = Graph()
+
+    for parent, child in ancestors:
+        if parent not in graph.vertices:
+            graph.add_vertex(parent)
+        if child not in graph.vertices:
+            graph.add_vertex(child)
+        # Build in reverse order
+        graph.add_edge(child, parent)
+
+    # Do a BFS (storing the path)
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        # If the path is longer or equal and the value is smaller, or if the path is longer)
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliest_ancestor
